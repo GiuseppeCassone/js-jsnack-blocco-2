60dcaa05b2601c73e5a74a2198dce638785118b2
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-02-05 13:38:23 +0100

    first commit

---
+++ snack1/js/script.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
+
+let arrayNumbers = [];
+
+let sum = 0;
+
+while (sum < 50){
+    
+    let userNumbers = Number(prompt("Inserisci un numero"));
+    arrayNumbers.push(userNumbers);
+    sum += userNumbers;
+
+}
+
+console.log(arrayNumbers);
